ce33187bc98bed170b22a7b25382ea736ff9adba
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2023-01-16 15:55:15 +0000

    Added more sections

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -23,15 +23,28 @@ class MainContent extends Component {
                             <input type='text' id='link' required placeholder='If you have a portfolio to show off'></input>
                     </fieldset>
                     <fieldset className='PersonalProfile'>
-                        <legend>*Personal Profile</legend>
-                            <label for='profile'>Personal Profile:</label>
+                        <legend>Personal Profile</legend>
+                            <label for='profile'>*Personal Profile:</label>
                             <textarea rows='8' id='profile' required placeholder="Please write a brief about yourself, think about who you are, what you're doing currently and looking to do in the future, and personalise it to the job you're applying for!"></textarea>
                     </fieldset>
                     <fieldset>
-                        <legend>Work Experience</legend>
+                        <legend>Employment History</legend>
+                            <label for='currentJob'>Curret Job</label>
+                            <input type='checkbox' id='currentJob' name='currentJob'></input>
+                            <label for='title'>Job Title:</label>
+                            <input type='text' id='title' placeholder='Job title'></input>
+                            <label for='company'>Company Name:</label>
+                            <input type='text' id='company' placeholder='Company name'></input>
+                            <label for='startDate'>start Date:</label>
+                            <input type='month' id='startDate'></input>
+                            <label for='endDate'>End Date:</label>
+                            <input type='month' id='endDate'></input>
+                            <label for='jobDescription'>Job Description</label>
+                            <textarea rows='3' type='text' id='jobDescription' placeholder='A brief description of your job, duties and anything related to the position your applying for.'></textarea>
                     </fieldset>
                     <fieldset>
                         <legend>Skills</legend>
+                            <p>A section to add skills and tag them to the inputed jobs above</p>
                     </fieldset>
                     <fieldset>
                         <legend>Education</legend>
