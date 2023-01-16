86f0215609f574973b0a3de3bf30f782391d5850
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2023-01-16 15:14:07 +0000

    Added more sections and formatting

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -10,14 +10,22 @@ class MainContent extends Component {
 
     render() {
         return (
-            <div className="MainContainer">
-                This is where the form will go to enter the info
+            <div className='MainContainer'>
+                <p>Feilds below marked with a * are required</p>
                 <form>
-                    <fieldset>
+                    <fieldset className='PersonalInfo'>
                         <legend>Personal Info</legend>
+                            <label for='name'>*Name:</label>
+                            <input type='text' id='name' required placeholder='First and last name'></input>
+                            <label for='email'>*Email:</label>
+                            <input type='text' id='email' required placeholder='Your email address'></input>
+                            <label for='link'>Weblink:</label>
+                            <input type='text' id='link' required placeholder='If you have a portfolio to show off'></input>
                     </fieldset>
-                    <fieldset>
-                        <legend>Summary</legend>
+                    <fieldset className='PersonalProfile'>
+                        <legend>*Personal Profile</legend>
+                            <label for='profile'>Personal Profile:</label>
+                            <textarea rows='8' id='profile' required placeholder="Please write a brief about yourself, think about who you are, what you're doing currently and looking to do in the future, and personalise it to the job you're applying for!"></textarea>
                     </fieldset>
                     <fieldset>
                         <legend>Work Experience</legend>
