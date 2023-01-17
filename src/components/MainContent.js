66c50b9e245dd8cdb5cf97c2e007f285d8b26194
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2023-01-17 15:00:00 +0000

    Added last sections with placeholders and marked where required

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -29,25 +29,40 @@ class MainContent extends Component {
                     </fieldset>
                     <fieldset>
                         <legend>Employment History</legend>
-                            <label for='currentJob'>Curret Job</label>
+                            <label for='currentJob'>*Curret Job</label>
                             <input type='checkbox' id='currentJob' name='currentJob'></input>
-                            <label for='title'>Job Title:</label>
+                            <label for='title'>*Job Title:</label>
                             <input type='text' id='title' placeholder='Job title'></input>
-                            <label for='company'>Company Name:</label>
+                            <label for='company'>*Company Name:</label>
                             <input type='text' id='company' placeholder='Company name'></input>
-                            <label for='startDate'>start Date:</label>
-                            <input type='month' id='startDate'></input>
-                            <label for='endDate'>End Date:</label>
-                            <input type='month' id='endDate'></input>
-                            <label for='jobDescription'>Job Description</label>
-                            <textarea rows='3' type='text' id='jobDescription' placeholder='A brief description of your job, duties and anything related to the position your applying for.'></textarea>
+                            <label for='startDate'>*Start Date:</label>
+                            <input type='date' id='startDate'></input>
+                            <label for='endDate'>*End Date:</label>
+                            <input type='date' id='endDate'></input>
+                            <label for='jobDescription'>*Job Description:</label>
+                            <textarea rows='3' id='jobDescription' placeholder='A brief description of your job, duties and anything related to the position your applying for.'></textarea>
+                            <label for='jobSkills'>*Skills learnt from job:</label>
+                            <textarea rows='5' id='jobSkill' placeholder='Write a few lines about the relevant skills you learnt or improved while at this job.'></textarea>
                     </fieldset>
                     <fieldset>
                         <legend>Skills</legend>
-                            <p>A section to add skills and tag them to the inputed jobs above</p>
+                            <label for='skills'>*Key Skills:</label>
+                            <textarea rows='5' id='skills' placeholder='Give some skills with examples that are relevant to the job application that may not be from previous jobs.'></textarea>
                     </fieldset>
                     <fieldset>
                         <legend>Education</legend>
+                            <label for='place'>*University/college/school:</label>
+                            <input type='text' id='place'></input>
+                            <label for='qualification'>*Qualification(s):</label>
+                            <input type='text' id='qualification' placeholder='Degree, BTEC, A-level, GCSE'></input>
+                            <label for='subject'>*Course/subject's:</label>
+                            <input type='text' id='subject' placeholder='Art, Math, IT, Physical Education'></input>
+                            <label for='grades'>*Grades:</label>
+                            <input type='text' id='grades' placeholder='2.0, A-C, Pass, 1-5'></input>
+                            <label for='completed'>*Date of completion:</label>
+                            <input type='date' id='completed'></input>
+                            <label for='eduDescription'>Description</label>
+                            <input type='text' id='eduDescription' placeholder='Optional description'></input>
                     </fieldset>
                 </form>
             </div>
