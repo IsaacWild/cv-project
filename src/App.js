cad37e585b7dc0ed0aa46c0999d968893dce961a
DirtyReboot <96993283+IsaacWild@users.noreply.github.com>
2023-01-25 09:23:54 +0000

    converted App.js to componant style not function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import './App.css';
-import Header from './components/Header.js';
-import MainContent from './components/MainContent.js';
-import Footer from './components/Footer.js';
+import "./App.css";
+import Header from "./components/Header.js";
+import MainContent from "./components/MainContent.js";
+import Footer from "./components/Footer.js";
+import React, { Component } from "react";
 
+class App extends Component {
+  constructor() {
+    super();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MainContent />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
